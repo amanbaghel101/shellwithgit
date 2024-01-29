687025192c5e68693bf593f5caea000866d49f7b
amanbaghel101 <amanbaghel333@gmail.com>
2024-01-29 12:13:17 +0530

    change age and upload

--- touch.sh
+++ touch.sh
@@ -30,9 +30,12 @@ then
 elif [ $age -ge 13 ] && [ $age -lt 19 ];
 then
 	echo "teenager"
-elif [ $age -ge 20 ];
+elif [ $age -ge 20 ] && [ $age -le 60 ];
 then
 	echo "Adult"
+elif [ $age -gt 60 ];
+then
+	echo "oldage"
 else
 	echo "invalid input"
 fi
